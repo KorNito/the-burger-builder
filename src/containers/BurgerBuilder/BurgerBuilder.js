152e55aec260bd3ea2f82b9086178e38c4fee552
KorNito <kornelijuspacevicius@gmail.com>
2021-03-22 23:09:48 +0200

    Bug fix(Order Summary not being shown)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -147,12 +147,14 @@ class BurgerBuilder extends Component {
           />
         </Auxiliary>
       );
-      <OrderSummary
-        ingredients={this.state.ingredients}
-        price={this.state.totalPrice}
-        purchaseCancelled={this.purchaseCancelHandler}
-        purchaseContinued={this.purchaseContinueHandler}
-      />;
+      orderSummary = (
+        <OrderSummary
+          ingredients={this.state.ingredients}
+          price={this.state.totalPrice}
+          purchaseCancelled={this.purchaseCancelHandler}
+          purchaseContinued={this.purchaseContinueHandler}
+        />
+      );
     }
 
     if (this.state.loading) {
